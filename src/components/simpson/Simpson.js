668bb2633e93e4740373ea8c6f752b8c45ea8624
Veronika-Uppie <veronikauppie@gmail.com>
2023-04-11 18:27:00 +0300

    Homework1

--- src/components/simpson/Simpson.js
+++ src/components/simpson/Simpson.js
@@ -4,9 +4,7 @@ const Simpson = ({item}) => {
             <div>{item.name} {item.surname}</div>
             <div>{item.age} років</div>
             <div>{item.info}</div>
-
-            {/*<img src="${value.photo}" alt="${value.name}">*/}
-            <div> .</div>
+            <img src={item.photo} alt={item.name}/>
         </div>
     );
 
