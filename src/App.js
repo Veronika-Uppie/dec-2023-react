0718eaa72c9624e4699268ca75995610b8b7c4df
Veronika-Uppie <veronikauppie@gmail.com>
2023-04-16 16:54:46 +0200

    Hw3

--- src/App.js
+++ src/App.js
@@ -1,89 +1,20 @@
-// з jsonplaceholder отримати всі пости в компоненту Posts.js
-// відобразити кожного інформацію (id,title) з кожного поста (компонента Post)
-// Зробити кнопку вибора поста, при натисканні на яку в Posts.js ви покажете детальну інфомацію про пост(всю інфу)
 //
-//
-//
-// =====
-// є API от SpaceX
-// https://api.spacexdata.com/v3/launches/
-//     потрібно вивести всі запуски кораблів окрім 2020 року
-// репрезентувати тільки окремі поля (зазначені в скрнішоті в папці)
-//
-//
-// #Advance
-//
-// вивести всіх юзерів з плайсхолдеру
-// в кожного юзера має бути кнопка яка буде показувати пости цього юзера
-// пости мають виводитись під компонетою Users (в App компоненті)
 
 
 
-
-import Posts from "./components/posts/Posts";
-import FullPost from "./components/fullPost/fullPost";
-import {useState} from "react";
-import Flights from "./components/flights/flights";
-import Users from "./components/users/users";
-import AllPosts from "./components/allPosts/allPosts";
 
 const App = () => {
 
-    let [chosenPost, setChosenPost] = useState(null);
 
-    let [userId, setUserId] = useState(null);
-
-
-
-
-    const lift =(obj) => {
-        setChosenPost({...obj});
-    }
 
 
 
   return (
 
-      <div className={'main'}>
+
+        <div></div>
 
 
-    <div className={'container'}>
-
-
-        <div className={'left-side'}>
-            <Posts lift = {lift}/>
-
-        </div>
-
-
-        {
-            chosenPost
-            &&
-            (<div className={'right-side'}>
-                <FullPost value={chosenPost}/>
-            </div>)
-        }
-
-    </div>
-
-          {
-
-              <div>
-                  <Flights/>
-              </div>
-
-          }
-
-
-              <Users setUserId={setUserId}/>
-
-          {
-              userId
-              &&
-              <AllPosts userId={userId}/>
-          }
-
-    </div>
   );
 }
 
