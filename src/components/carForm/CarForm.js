1c86b04fcea711a026f85b18a45cd255a4d6998c
Veronika-Uppie <veronikauppie@gmail.com>
2023-04-19 13:52:01 +0200

    master

--- src/components/carForm/CarForm.js
+++ src/components/carForm/CarForm.js
@@ -1,9 +1,11 @@
 import React, {useEffect} from 'react';
 import {useForm} from "react-hook-form";
 import {carService} from "../../servises/car.cervice";
+import {joiResolver} from '@hookform/resolvers/joi'
+import {carValidator} from "../../validators/car.validator";
 
 const CarForm = ({setAllCars, carForUpdate}) => {
-    const {register, handleSubmit, reset, formState: {errors, isValid}, setValue} = useForm({mode: 'all'});
+    const {register, handleSubmit, reset, formState: {errors, isValid}, setValue} = useForm({mode:'all', resolver:joiResolver(carValidator) });
     useEffect(()=>{
         if (carForUpdate){
             setValue('brand', carForUpdate.brand, {shouldValidate:true})
@@ -24,27 +26,12 @@ const CarForm = ({setAllCars, carForUpdate}) => {
 
     return (
         <form onSubmit={handleSubmit(carForUpdate?update:save)}>
-            <input  placeholder={'brand'} {...register('brand',
-                {pattern: {
-                        value:/^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/,
-                        message: 'Бренд має складатись з літер та містити 1-20 символів'
-                    },
-                    required:{value:true}
-                })}/>
+            <input  placeholder={'brand'} {...register('brand')}/>
             {errors.brand&&<span>{errors.brand.message}</span>}
-            <input  placeholder={'price'} {...register('price', {
-                valueAsNumber:true,
-                min: {value: 0, message: 'Мінімальне значення 0'},
-                max: {value: 1000000, message: 'Максимальне значення 1000000'},
-                required:{value:true}
-            })}/>
+            <input  placeholder={'price'} {...register('price')}/>
             {errors.price&& <span>{errors.price.message}</span>}
-            <input  placeholder={'year'} {...register('year', {
-                valueAsNumber:true,
-                min: {value: 1990, message: 'Мінімум 1990 рік'},
-                max: {value: new Date().getFullYear(), message: 'Максімум поточний рік'},
-                required:{value:true}
-            })} />
+            <input  placeholder={'year'} {...register('year')} />
+            {errors.year&& <span>{errors.year.message}</span>}
 
             <input type={"submit"} value={carForUpdate?'Update':'Create'} disabled={!isValid}/>
         </form>
--- src/validators/car.validator.js
+++ src/validators/car.validator.js
@@ -1,9 +1,22 @@
 import Joi from "joi";
 
-Joi.object({
+const carValidator = Joi.object({
     brand:Joi.string().regex(/^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/).required().messages({
         'string.pattern.base':'Бренд має складатись з літер та містити 1-20 символів',
         'string.required':'Це поле обов\'язкове'
     }),
-    price:Joi.number().min()
-})
+    price:Joi.number().min(0).max(1000000).required().messages({
+        'number.min':'Мінімальне значення 0',
+        'number.max':'Максимальне значення 1000000',
+        'number.required':'Це поле обов\'язкове'
+    }),
+    year:Joi.number().min(1990).max(new Date().getFullYear()).required().messages({
+        'number.min':'Мінімум 1990 рік',
+        'number.max':'Максімум поточний рік',
+        'number.required':'Це поле обов\'язкове'
+    })
+});
+
+export {
+    carValidator
+}
